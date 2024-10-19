9a68d77c88f344dac42a11b28dd365d9bbcc1602
AmadFat <1943269060@qq.com>
2024-10-19 21:18:48 +0800

    Update training.

--- rtdetrv2_pytorch/run.sh
+++ rtdetrv2_pytorch/run.sh
@@ -1,1 +1,1 @@
-python tools/train.py -c configs/rtdetrv2/rtdetrv2_r18vd_tune.yml -t /path/to/rtdetrv2_r18.pth --use-amp
+python tools/train.py -c configs/rtdetrv2/rtdetrv2_r18vd_tune.yml -t rtdetrv2_r18.pth --use-amp
